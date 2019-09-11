c8a57741412d4a081350247ae88c123b1853903b
Aman Jha <aman.jha@here.com>
2019-09-12 00:25:38 +0530

    Update serializer interface to use generics instead of hardbound class
    Change-Id: Id5941c23ce19d6441eadb48b18a08fa1c4459288

--- src/main/java/com/here/object/cache/serializer/Serializer.java
+++ src/main/java/com/here/object/cache/serializer/Serializer.java
@@ -3,7 +3,7 @@ package com.here.object.cache.serializer;
 import java.io.Serializable;
 
 public interface Serializer {
-	byte[] serialize(Serializable object);
+	<T extends Serializable> byte[] serialize(T object);
 
 	<T> T deserialize(byte[] objectData);
 }
